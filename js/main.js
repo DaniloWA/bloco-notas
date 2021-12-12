4a0ad45dbd776ebf312be92b108c6aa8ddd4ce7a
Danilo Oliveira <DDanilo000@gmail.com>
2021-12-12 13:33:14 +0000

    criar element
    criar element
    adc element
    contador

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+let contadorNumero = Number(0)
 
-function temas(){
+
+function temas(){ //Escolhe entre Dark e Light
+
     let tema = document.getElementsByTagName('html')[0].getAttribute('tema');
     let temaVariavel = document.getElementsByTagName('html')[0]
     let temaTxt = document.getElementById("temaTxt");
@@ -17,3 +20,111 @@ function temas(){
     }
 }
 
+function getElementoPai(){ //Pega o elemento pai
+    return document.getElementById("pai")
+}
+
+function adcElemento(){ //Cria e Adiciona os elementos novos
+    let div = getElementoPai()
+  
+    //Criando elemento  
+    let data = document.createElement("p")
+    let divB = document.createElement("div")
+    let inputTXT = document.createElement("input")
+    let hr = document.createElement("hr")
+    let a1 = document.createElement("a")
+    let a2 = document.createElement("a")
+    let img1 = document.createElement("img")
+    let img2 = document.createElement("img")
+    let txtArea = document.createElement("textarea")
+
+    divB.className = "bloco"
+
+    data.id = "data"
+
+    inputTXT.type = "text"
+    inputTXT.autocomplete = "off"
+    inputTXT.maxLength = "13"
+    inputTXT.className = "titulo"
+    inputTXT.id = "titulo"
+    inputTXT.placeholder = "Titulo"
+
+    hr.className = "hrBloco"
+
+    a1.href="#" 
+    a1.className="imgBlocoLapis"
+
+    a2.href="#" 
+    a2.className="imgBlocoLixo"
+
+
+
+    img1.src="img/pencil-fill.svg"
+    img1.alt="lapis"
+
+    img2.src="img/trash-fill.svg" 
+    img2.alt="lixo"
+
+    txtArea.className="textAreaBloco" 
+    txtArea.name="textArea" 
+    txtArea.id="textArea"
+    txtArea.placeholder = "Escrava sua Nota!"
+    
+    //Adicionando elemenntos
+    div.appendChild(divB)
+    divB.appendChild(data)
+    divB.appendChild(inputTXT)
+    divB.appendChild(hr)
+    divB.appendChild(a1)
+    a1.appendChild(img1)
+    divB.appendChild(a2)
+    a2.appendChild(img2)
+    divB.appendChild(txtArea)
+
+    data.innerHTML = getHora()
+    adicionarNumero()
+    excluir()
+}
+
+function getHora() { //Pega a Data atual
+    let data    = new Date()
+    var dia     = data.getDate();          
+    var hora    = data.getHours();        
+    var mes     = data.getMonth();        
+    var ano     = data.getFullYear();        
+    var minutos = data.getMinutes();
+    
+    var str_hora = `<b> ${dia}/${mes}/${ano} <br>${hora}:${minutos}<b> `
+    
+    return str_hora
+}
+
+function adicionarNumero(){
+
+    let txtContador = document.getElementById("ContadorHTML")
+
+    contadorNumero ++
+
+    console.log("Adicionar : + " + contadorNumero)
+
+    txtContador.innerHTML = contadorNumero
+}
+
+function excluir(){
+    let txtContador = document.getElementById("ContadorHTML")
+    let lixo = document.getElementsByClassName("imgBlocoLixo")
+    let lixos = [] 
+
+    lixos.push("Contador : " + contadorNumero + " : " + lixo[2])
+    console.log("Lixos : - " + lixos)
+    /* 
+    lixo[contadorNumero].addEventListener("click", function(){
+
+        console.log("Excluir : - " + contadorNumero)
+
+        contadorNumero --   
+        txtContador.innerHTML = contadorNumero 
+      });
+      */
+
+}
